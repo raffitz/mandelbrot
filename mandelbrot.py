9b3b84bccd952881a2a03cb0ec76aff35f71571c
raffitz <raf.a.m.c.gon@gmail.com>
2015-11-01 23:26:10 +0000

    Added window zooming, rescaling and resetting

--- mandelbrot.py
+++ mandelbrot.py
@@ -25,6 +25,12 @@ miny = -1
 width = 3.5
 height = 2
 
+cursor1x = 0
+cursor1y = 0
+cursorenabled = False
+cursor2x = 0
+cursor2y = 0
+
 def getcoords(i,j):
 	a = (i / 896.0)*width+minx
 	b = ((512-j)/512)*height+miny
@@ -39,21 +45,80 @@ intermediate = pygame.Surface((896,512),0)
 imajor = 0
 jmajor = 0
 
-blocksize = 64;
+blocksize = 64
 
 ibound = round(896/blocksize)
 jbound = round(512/blocksize)
 
 surface.lock()
 
+reset = False
+
 while running:
 	
 	for ebuddy in pygame.event.get():
-		if ebuddy.type == pygame.key:
+		if ebuddy.type == pygame.KEYDOWN:
 			if ebuddy.key ==pygame.K_ESCAPE:
 				running = False
+			elif ebuddy.key == pygame.K_BACKSPACE:
+				minx = -2.5
+				miny = -1.0
+				width = 3.5
+				height = 2.0
+				reset = True
+			elif ebuddy.key == pygame.K_SPACE:
+				ratio = width/height
+				if ratio<7/4:
+					width = height*(7/4)
+					reset = True
+				elif ratio>7/4:
+					height = (4/7)*width
+					reset = True
+			elif ebuddy.key == pygame.K_MINUS:
+				minx = minx - width/2
+				miny = miny - height/2
+				width = width*2
+				height = height*2
+				reset = True
+			elif ebuddy.key == pygame.K_MINUS:
+				minx = minx - width/2
+				miny = miny - height/2
+				width = width*2
+				height = height*2
+				reset = True
+			elif ebuddy.key == pygame.K_PLUS:
+				minx = minx + width/4
+				miny = miny + height/4
+				width = width/2
+				height = height/2
+				reset = True
+			if reset:
+				reset = False
+				blocksize = 64
+				ibound = round(896/blocksize)
+				jbound = round(512/blocksize)
+				imajor = 0
+				jmajor = 0
 		elif ebuddy.type == pygame.QUIT:
 			running = False
+		elif ebuddy.type == pygame.MOUSEBUTTONUP:
+			if ebuddy.button == 1:
+				cursorenabled = True
+				(cursor1x,cursor1y) = ebuddy.pos
+			if ebuddy.button == 3 and cursorenabled:
+				cursorenabled = False
+				(cursor2x,cursor2y) = ebuddy.pos
+				(rx1,ry1) = getcoords(cursor1x,cursor1y)
+				(rx2,ry2) = getcoords(cursor2x,cursor2y)
+				minx = min(rx1,rx2)
+				miny = min(ry1,ry2)
+				width = abs(rx1-rx2)
+				height = abs(ry1-ry2)
+				blocksize = 64
+				ibound = round(896/blocksize)
+				jbound = round(512/blocksize)
+				imajor = 0
+				jmajor = 0
 	if blocksize >= 1:
 		if blocksize != 64 and imajor%2==0 and jmajor%2 ==0:
 			skip = True
@@ -95,10 +160,15 @@ while running:
 				surface.unlock()
 				intermediate.blit(surface,(0,0),None,0)
 				disps.blit(intermediate,(0,0),None,0)
+				if cursorenabled:
+					disps.set_at((cursor1x,cursor1y),(255,0,0))
 				pygame.display.update()
 				surface.lock()
 	else:
 		clock.tick(30)
+		disps.blit(intermediate,(0,0),None,0)
+		if cursorenabled:
+			disps.set_at((cursor1x,cursor1y),(255,0,0))
 		pygame.display.update()
 pygame.quit()
 quit()
